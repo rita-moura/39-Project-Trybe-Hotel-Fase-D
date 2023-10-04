e3c5ea821842c4e99b725e681612cd7c87c50907
Rita Moura <ritamoura520@gmail.com>
2023-10-04 16:13:53 -0300

    Feat: adiciona O método get na rota /

--- src/TrybeHotel/Controllers/StatusController.cs
+++ src/TrybeHotel/Controllers/StatusController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 
 namespace TrybeHotel.Controllers
@@ -7,6 +8,24 @@ namespace TrybeHotel.Controllers
     [Route("/")]
     public class StatusController : Controller
     {
-    
+        private readonly ILogger<StatusController> _logger;
+
+        public StatusController(ILogger<StatusController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                return Ok((new { message = "online" }));
+            }
+            catch (ApplicationException ex)
+            {
+                return Conflict(new { message = ex.Message });
+            }
+        }
     }
 }
